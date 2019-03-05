cf4ed526ebb48ea72319b891050cb27f26321281
Sarai <saraiflores@gmail.com>
2019-03-05 15:51:24 -0500

    adding preliminary code

---
+++ react-client/src/Components/AboutMe.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class AboutMe extends Component {
+  render() {
+    return (
+      <div>
+        This is who I am
+      </div>
+    )
+  }
+}
+export default AboutMe;
---
+++ react-client/src/Components/HomePage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        Home page
+        I AM GREAT
+      </div>
+    )
+  }
+}
+export default HomePage;
